3b932f5b9067c27fd5b59ff7de82376747f9e836
Deadpool1 <atwood169@yahoo.com>
2023-05-24 17:23:20 -0500

    append

--- index.js
+++ index.js
@@ -11,10 +11,29 @@ const LinkedList = () => {
 
     const append = (value) => {
         const newNode = nodeList(value)
+        if (head === null) {
+            head = newNode
+        } else {
+            let pointer = head
+            while (pointer.nextNode !== null) {
+                pointer = pointer.nextNode
+            }
+            pointer.nextNode = newNode
+        }
+        size++
     }
 
 
     const prepend = (value) => {
         const newNode = nodeList(value, head)
     }
+
+    return {
+        append
+    }
 }
+
+const testList = LinkedList()
+testList.append(5)
+
+console.log(testList.size)
