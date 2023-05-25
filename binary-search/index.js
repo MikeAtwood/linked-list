894268491898301fb37ddc916b167b65541318bd
Deadpool1 <atwood169@yahoo.com>
2023-05-25 17:12:56 -0500

    started tree function

--- binary-search/index.js
+++ binary-search/index.js
@@ -1,6 +1,14 @@
 // Factory function for Node
-const Node = () => {
-    value,
-    leftChild,
-    rightChild
+const Node = (value = null, leftChild = null, rightChild = null) => {
+    return {
+        value,
+        leftChild,
+        rightChild
+    }
+}
+
+// TODO: build a tree factory function which accepts an array when initialized
+// Tree Factory Function
+const Tree = () => {
+    
 }
