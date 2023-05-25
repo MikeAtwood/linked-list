b5a8a7f56a0c9987c40ba0dedd3e0316bfec4c73
Deadpool1 <atwood169@yahoo.com>
2023-05-25 13:59:20 -0500

    updated linked list

--- index.js
+++ index.js
@@ -40,13 +40,91 @@ const LinkedList = () => {
         }
         length++
     }
-
+    
+    // Get size of linked list
     const size = () => length 
+
+    const headNode = () => {
+        if (head === null) {
+            console.log("There's no head node")
+        } else {
+            return head
+        }
+    }
+    // Shows node at end of the list
+    const tail = () => {
+        if(head === null) {
+            console.log("There's no tail")
+        } else {
+            let pointer = head
+            while (pointer.nextNode !== null) {
+                pointer = pointer.nextNode
+            }
+            return pointer
+        }
+    }
+
+    // Returns node at specified index
+    const at = (index) => {
+        if (head === null) {
+            console.log("There is no linked list")
+        } else {
+            let pointer = head
+            for (let i = 0; i < index; i++) {
+                if (pointer.nextNode === null) {
+                    return "There's no item at that index"
+                } else {
+                    pointer = pointer.nextNode
+                }
+            }
+            return pointer
+        }
+    }
+    
+    // Removes node from end of the list
+    const pop = () => {
+        if (head === null) {
+            console.log("There's no head node")
+        } else {
+            let pointer = head
+            while (pointer.nextNode.nextNode !== null) { // Looks two nodes ahead to check if not = to null
+                pointer = pointer.nextNode
+            }
+            pointer.nextNode = null
+            length--
+        }
+    }
+
+    // Checks if value is in the list
+    const contains = (value) => {
+        if (head === null) {
+            return false
+        } else {
+            let pointer = head
+            while (pointer.nextNode !== null) {
+                if (pointer.value === value) {
+                    return true
+                }
+                pointer = pointer.nextNode
+            }
+            // Check if the value is true or false
+            if (pointer.nextNode !== value) {
+                return false
+            } else {
+                return true
+            }
+        }
+    }
 
     return {
         append,
         prepend,
-        size
+        size,
+        headNode,
+        tail,
+        at,
+        pop,
+        contains
     }
 }
 
