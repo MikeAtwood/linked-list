61b2b7e326194ffc923e2de67b42a12256b2ac92
Deadpool1 <atwood169@yahoo.com>
2023-05-28 13:42:41 -0500

    updated insertNode

--- binary-search/index.js
+++ binary-search/index.js
@@ -27,31 +27,33 @@ const Tree = (array) => {
     const root = buildTree(array)
     
     // Method to add node to array
-    const insertNode = (value, node) => {
+    const insertNode = (value, node = root) => {
         let newNode = Node(value)
         if (root === null) {
-            return newNode
+          return newNode
         }
         // Traverse the tree to find position for the new node
         let prev = null
         let pointer = node
         while (pointer !== null) {
-            prev = pointer;
-            if (pointer && value < pointer.value) {
-                pointer = pointer.leftChild
-            } else if (pointer && value > pointer.value) {
-                pointer = pointer.rightChild
-            } else {
-                return node
-            }
+          prev = pointer
+          if (value < pointer.value) {
+            pointer = pointer.leftChild
+          } else if (value > pointer.value) {
+            pointer = pointer.rightChild
+          } else {
+            // Handle case where node with same value already exists
+            return node
+          }
         }
+        // Add the new node to the tree
         if (value < prev.value) {
-            prev.leftChild = newNode
+          prev.leftChild = newNode
         } else {
-            prev.rightChild = newNode
+          prev.rightChild = newNode
         } 
         return node
-    }
+      }
 
 
     // Delete to remove node from array TODO
@@ -82,7 +84,6 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
 
   // node test
 let test = Tree([54, 32, 66, 133, 5, 25, 73, 43, 342])
-root = insertNode(44, root)
+test.insertNode(44)
 //let test = Tree([1, 2, 3, 4, 5, 6, 7, 8, 9])
 prettyPrint(test.root)
-prettyPrint(root) 
